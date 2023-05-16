1140655c7b6c47af42c038beb0a15e4f8ede1dcb
evgeniy210191 <88433785+evgeniy210191@users.noreply.github.com>
2023-05-16 17:05:04 +0000

    Deploying to gh-pages from @ evgeniy210191/project-final@90f4fd235d18b23a4995c9b7de1fa69dc3721fdf 🚀

---
+++ index.b14fe2fb.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".modal-menu"),o=document.querySelector(".mobile-menu");e.addEventListener("click",(function(){e.classList.toggle("is-open"),o.classList.toggle("is-open")}));
+//# sourceMappingURL=index.b14fe2fb.js.map
