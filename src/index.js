8f388a006f4174775b1713e150e6b74328150bfd
evgeniy210191 <bees127@ukr.net>
2023-05-21 22:07:56 +0300

    active

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 const btn = document.querySelector('.modal-menu');
 const menu = document.querySelector('.mobile-menu');
+const list = menu.querySelector('ul');
+const body = document.querySelector('body');
 
 function addClassBtn() {
   btn.classList.toggle('is-open');
   menu.classList.toggle('is-open');
 }
+function removeClassBtn(e) {
+  if (e.target.localName === 'a') {
+    btn.classList.remove('is-open');
+    menu.classList.remove('is-open');
+  }
+}
+
+function openModal(event) {
+  if (event.target.localName === 'button') {
+    if (event.target.hasAttribute('data-atribut')) {
+      if (event.target.dataset.atribut === 'close') {
+        return document
+          .querySelector(`.backdrop.is-open`)
+          .classList.remove('is-open');
+      }
+
+      document
+        .querySelector(`.backdrop.${event.target.dataset.atribut}`)
+        .classList.add('is-open');
+    }
+  }
+}
+
+body.addEventListener('click', openModal);
 
 btn.addEventListener('click', addClassBtn);
+list.addEventListener('click', removeClassBtn);
