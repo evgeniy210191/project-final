4e3db9ffd9f1f344bfc7dd3ac572a49fdfe287e6
evgeniy210191 <bees127@ukr.net>
2023-05-22 14:32:00 +0300

    fixed js

--- src/index.js
+++ src/index.js
@@ -2,35 +2,46 @@ const btn = document.querySelector('.modal-menu');
 const menu = document.querySelector('.mobile-menu');
 const list = menu.querySelector('ul');
 const body = document.querySelector('body');
+const root = document.querySelector('.root');
 
 function addClassBtn() {
   btn.classList.toggle('is-open');
   menu.classList.toggle('is-open');
+  body.classList.toggle('is-scroll');
 }
+
 function removeClassBtn(e) {
   if (e.target.localName === 'a') {
     btn.classList.remove('is-open');
     menu.classList.remove('is-open');
+    body.classList.remove('is-scroll');
+  }
+}
+
+function closeModal(event) {
+  console.log(event.target);
+  if (event.target.localName === 'button') {
+    if (event.target.hasAttribute('data-atribut')) {
+      if (event.target.dataset.atribut === 'close') {
+        document.querySelector('.backdrop.is-open').classList.remove('is-open');
+        body.classList.remove('is-scroll');
+      }
+    }
   }
 }
 
 function openModal(event) {
   if (event.target.localName === 'button') {
     if (event.target.hasAttribute('data-atribut')) {
-      if (event.target.dataset.atribut === 'close') {
-        return document
-          .querySelector(`.backdrop.is-open`)
-          .classList.remove('is-open');
-      }
-
       document
         .querySelector(`.backdrop.${event.target.dataset.atribut}`)
         .classList.add('is-open');
+      body.classList.add('is-scroll');
+      backdrop = document.querySelector('.backdrop.is-open');
     }
   }
 }
-
 body.addEventListener('click', openModal);
-
+root.addEventListener('click', closeModal);
 btn.addEventListener('click', addClassBtn);
 list.addEventListener('click', removeClassBtn);
