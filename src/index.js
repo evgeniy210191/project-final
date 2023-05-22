1f854c482cdaa6b26a0b4dd103aeaf02d39dac4c
evgeniy210191 <bees127@ukr.net>
2023-05-22 17:29:10 +0300

    end

--- src/index.js
+++ src/index.js
@@ -1,47 +1,40 @@
+'use strict';
 const btn = document.querySelector('.modal-menu');
 const menu = document.querySelector('.mobile-menu');
 const list = menu.querySelector('ul');
 const body = document.querySelector('body');
-const root = document.querySelector('.root');
 
 function addClassBtn() {
   btn.classList.toggle('is-open');
   menu.classList.toggle('is-open');
-  body.classList.toggle('is-scroll');
+  body.classList.toggle('not-scroll');
 }
 
 function removeClassBtn(e) {
   if (e.target.localName === 'a') {
     btn.classList.remove('is-open');
     menu.classList.remove('is-open');
-    body.classList.remove('is-scroll');
+    body.classList.remove('not-scroll');
   }
 }
 
-function closeModal(event) {
-  console.log(event.target);
+function toggleModal(event) {
   if (event.target.localName === 'button') {
-    if (event.target.hasAttribute('data-atribut')) {
-      if (event.target.dataset.atribut === 'close') {
-        document.querySelector('.backdrop.is-open').classList.remove('is-open');
-        body.classList.remove('is-scroll');
-      }
+    if (event.target.dataset.atribut === 'close') {
+      document.querySelector('.backdrop.is-open').classList.remove('is-open');
+      body.classList.remove('not-scroll');
     }
-  }
-}
-
-function openModal(event) {
-  if (event.target.localName === 'button') {
-    if (event.target.hasAttribute('data-atribut')) {
+    if (
+      event.target.hasAttribute('data-atribut') &&
+      event.target.dataset.atribut !== 'close'
+    ) {
       document
-        .querySelector(`.backdrop.${event.target.dataset.atribut}`)
+        .querySelector(`.${event.target.dataset.atribut}`)
         .classList.add('is-open');
-      body.classList.add('is-scroll');
-      backdrop = document.querySelector('.backdrop.is-open');
+      body.classList.add('not-scroll');
     }
   }
 }
-body.addEventListener('click', openModal);
-root.addEventListener('click', closeModal);
+body.addEventListener('click', toggleModal);
 btn.addEventListener('click', addClassBtn);
 list.addEventListener('click', removeClassBtn);
