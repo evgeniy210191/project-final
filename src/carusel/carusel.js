a2df94e56a18111ececd49834380e1bb0fb01b53
evgeniy210191 <bees127@ukr.net>
2023-05-24 17:45:25 +0300

    not animation

--- src/carusel/carusel.js
+++ src/carusel/carusel.js
@@ -3,17 +3,27 @@ const canvas = document.querySelector('.review');
 const conteinerElements = document.querySelector('.reviews-container');
 
 export const carusel = {
-  step: canvas.getBoundingClientRect().width + 15,
+  position: 0,
+  step: canvas.getBoundingClientRect().width + 30,
   scrollSlide(evt) {
     evt.preventDefault();
     if (evt.target.dataset.atribut === 'step') {
       const clickedButton = Object.values(buttons);
-      carusel.position = Math.abs(
-        clickedButton.indexOf(evt.target) * carusel.step
-      );
+      position = Math.abs(clickedButton.indexOf(evt.target) * carusel.step);
       canvas.querySelector('.active').classList.remove('active');
-      conteinerElements.style.right = `${carusel.position}px`;
       evt.target.className = 'active';
+      // moving = setTimeout(movingElement, 20);
+      conteinerElements.style.right = `${position}px`;
     }
   },
 };
+function movingElement() {}
+
+return new Promise(res => {
+  if (carusel.position === position) {
+    clearInterval(moving);
+  } else {
+    res(carusel.position++);
+    conteinerElements.style.right = `${carusel.position}px`;
+  }
+});
